ebeeedbcfe196873a3022ea0da33e2d17926a7d8
JOAO <JOAO@JOAO>
2020-05-07 11:16:01 -0300

    Criaçao das entidades

---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descriçao { get; set; }
+        public string Preco { get; set; }
+    }
+}
